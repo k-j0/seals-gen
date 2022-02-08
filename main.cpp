82cc5e5337b84a7b30a44e36a408161d6c9a05f9
Jon <jonathan-kings@hotmail.com>
2022-02-08 12:57:06 +0100

    Implement delaunay triangulation example (js)

--- main.cpp
+++ main.cpp
@@ -8,14 +8,15 @@ int main() {
 	printf("Starting...\n\n");
 	
 	Surface::Params params;
+	params.attractionMagnitude = 1;
 	Surface surface(params, 0);
 
 	std::chrono::high_resolution_clock clock;
 	auto start = clock.now();
 
 	// grow progressively
-	// 10k iterations, serial, release build: ~190-240s (~3-4 mins) (of which tessellation ~.03s)
-	const int iterations = 1000;
+	// 10k iterations, serial, release build: ~190-240s (~3-4 mins) (of which tessellation ~.02s)
+	const int iterations = 10000;
 	std::string snapshotsJson = "[\n";
 	bool first = true;
 	for (int t = 0; t < iterations; ++t) {
@@ -24,17 +25,17 @@ int main() {
 		if (t % 5 == 0) {
 			surface.addParticle();
 		}
-		surface.update();
+		//surface.update();
 
 		// recurrent outputs (console + snapshots)
 		if (t % (iterations / 255) == 0) { // 255 hits over the full generation (no matter iteration count)
-			printf("%d %%...\r", t * 100 / iterations);
-			if (!first) {
+			//printf("%d %%...\r", t * 100 / iterations);
+			/*if (!first) {
 				snapshotsJson += ",\n";
 			}
 			snapshotsJson += surface.toJson();
 			first = false;
-			File::Write("results/surface.json", snapshotsJson + "\n]");
+			File::Write("results/surface.json", snapshotsJson + "\n]");*/
 		}
 
 	}
@@ -42,7 +43,7 @@ int main() {
 
 	// settle (iterations without new particles)
 	for (int t = 0; t < 50; ++t) {
-		surface.update();
+	//	surface.update();
 	}
 
 
@@ -50,7 +51,7 @@ int main() {
 	printf("Total runtime: %d ms.\n", int(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()));
 
 	// Write the final snapshot
-	snapshotsJson += ",\n" + surface.toJson();
+	snapshotsJson += (first ? "" : ",\n") + surface.toJson();
 	File::Write("results/surface.json", snapshotsJson + "\n]");
 
 	return 0;
