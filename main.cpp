8a8495f74761298ba9d09947746e4dcc224aa037
Jonathan Kings <jonathan-kings@hotmail.com>
2022-09-28 09:05:34 +0200

    Support more command-line params

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Surface3.h"
 #include "Surface2.h"
 #include "File.h"
+#include "SphereBoundary.h"
 #include "CylinderBoundary.h"
 #ifdef _OPENMP
 	#include <omp.h>
@@ -19,12 +20,68 @@ WARNING_DISABLE_OMP_PRAGMAS;
 int main(int argc, char** argv) {
 	
 	// Read arguments
-	int d, iterations;
+	SurfaceBase* surface = nullptr;
+	int iterations;
 	bool writeJson;
 	std::string outFile;
 	{
 		Arguments args(argc, argv);
-		d = args.read<int>("d", 2);
+		int d = args.read<int>("d", 2);
+		if (d == 3) {
+			Surface3::Params params;
+			params.attractionMagnitude = args.read<real_t>("magnitude", .025);
+			params.repulsionMagnitudeFactor = args.read<real_t>("repulsion", 2.1);
+			params.damping = args.read<real_t>("damping", .15);
+			params.noise = args.read<real_t>("noise", .25);
+			real_t aniso = args.read<real_t>("anisotropy", 1);
+			params.repulsionAnisotropy = Vec3(1.0, aniso, aniso);
+			std::string boundaryType = args.read<std::string>("boundary", "cylinder");
+			if (boundaryType.compare("cylinder") == 0) {
+				params.boundary = std::make_shared<CylinderBoundary>(
+					args.read<real_t>("boundary-radius", .15),
+					args.read<real_t>("boundary-max-radius", .15),
+					args.read<real_t>("boundary-extent", .05),
+					args.read<real_t>("boundary-growth", 0)
+				);
+			} else if (boundaryType.compare("sphere") == 0) {
+				params.boundary = std::make_shared<SphereBoundary<3>>(
+					args.read<real_t>("boundary-radius", .15),
+					args.read<real_t>("boundary-max-radius", .15),
+					args.read<real_t>("boundary-extent", .05),
+					args.read<real_t>("boundary-growth", 0)
+				);
+			}
+			Surface3::SpecificParams specificParams;
+			specificParams.attachFirstParticle = args.read<bool>("attach-first", false);
+			std::string growthStrategy = args.read<std::string>("growth-strategy", "delaunay");
+			if (growthStrategy.compare("edge") == 0) {
+				specificParams.strategy = Surface3::GrowthStrategy::ON_EDGE;
+			} else if (growthStrategy.compare("delaunay-aniso-edge") == 0) {
+				specificParams.strategy = Surface3::GrowthStrategy::DELAUNAY_ANISO_EDGE;
+			} else {
+				specificParams.strategy = Surface3::GrowthStrategy::DELAUNAY;
+			}
+			surface = new Surface3(params, specificParams, args.read<int>("seed", 0));
+		} else if (d == 2) {
+			Surface2::Params params;
+			params.attractionMagnitude = args.read<real_t>("magnitude", .01);
+			params.repulsionMagnitudeFactor = args.read<real_t>("repulsion", 2.1);
+			params.damping = args.read<real_t>("damping", .5);
+			params.noise = args.read<real_t>("noise", .25);
+			params.boundary = std::make_shared<SphereBoundary<2>>(
+				args.read<real_t>("boundary-radius", .5),
+				args.read<real_t>("boundary-max-radius", .5),
+				args.read<real_t>("boundary-extent", .05),
+				args.read<real_t>("boundary-growth", 0)
+			);
+			params.dt = 0.5;
+			Surface2::SpecificParams specificParams;
+			specificParams.attachFirstParticle = args.read<bool>("attach-first", false);
+			surface = new Surface2(params, specificParams);
+		} else {
+			printf("Error: invalid dimensionality %d! Must select 2 or 3.", d);
+			exit(1);
+		}
 		iterations = args.read<int>("iter", 600);
 		writeJson = args.read<bool>("json", false);
 		outFile = args.read<std::string>("out", "results/surface.bin");
@@ -39,34 +96,6 @@ int main(int argc, char** argv) {
 		printf("OpenMP enabled, %d threads.\n\n", omp_get_num_threads());
 	}
 #endif
-	
-	SurfaceBase* surface = nullptr;
-
-	if (d == 3) {
-		Surface3::Params params;
-		#ifdef NO_UPDATE
-			params.attractionMagnitude = 1.0;
-		#endif
-		params.repulsionAnisotropy = Vec3(1.0, 1.0, 1.0);
-		params.boundary = std::make_shared<CylinderBoundary>((real_t).15, (real_t).15);
-		surface = new Surface3(params, { true, Surface3::GrowthStrategy::DELAUNAY }, 0);
-
-	} else if (d == 2) {
-
-		Surface2::Params params;
-		params.attractionMagnitude = (real_t)0.01;
-		params.damping = (real_t)0.5;
-		params.dt = (real_t)0.5;
-		#ifdef NO_UPDATE
-			params.attractionMagnitude = (real_t)1.0;
-		#endif
-		params.boundary = std::make_shared<SphereBoundary<2>>((real_t)0.075, (real_t)0.5, (real_t).05, real_t(1 + 2e-4));
-		surface = new Surface2(params, { true, (real_t)1.35 }, 0);
-
-	} else {
-		printf("Error: invalid dimensionality! Must select 2 or 3.");
-		exit(1);
-	}
 	
 	std::string snapshotsJson = writeJson ? "[\n" : "";
 	std::vector<uint8_t> snapshotsBinary;
